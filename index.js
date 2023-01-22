e7d691b82efe2191655cb71ee7d4d2096edccdc3
wgao23 <wgao23@students.cs.ubc.ca>
2023-01-22 12:52:21 -0800

    Allow multiple recordings to be uploaded at once

--- index.js
+++ index.js
@@ -1,42 +1,48 @@
 function handleOnChange() {
     var inputElement = document.getElementById("input");
-    var audio = document.createElement("audio");
-    var file = inputElement.files[0];
-    var url = URL.createObjectURL(file);
+    var files = inputElement.files;
+    var formData = new FormData();
 
-    if(file.size > 5000000) {
-       window.alert("Your file size is too large!  Please ensure that your recording is less than 5MB before uploading again.");
-       return;
-    }
+    for (var i = 0; i < files.length; i++) {
+      var url = URL.createObjectURL(files[i]);
 
-    if(file.type !== "audio/wav") {
-        window.alert("Your file is not a .wav file!  Our analysis tool only accepts 16khz mono .wav files.");
+      if(files[i].size > 5000000) {
+        window.alert("Your file size is too large!  Please ensure that your recording is less than 5MB before uploading again.");
         return;
+      }
+
+      if(files[i].type !== "audio/wav") {
+          window.alert("Your file is not a .wav file!  Our analysis tool only accepts 16khz mono .wav files.");
+          return;
+      }
+
+      formData.append('files', files[i]);
+
+      var audio = document.createElement("audio");
+      audio.setAttribute("controls", "controls");
+      audio.setAttribute("src", url);
+      audio.setAttribute("type", "audio/wav");
+
+      var deleteButton = document.createElement("button");
+      deleteButton.setAttribute("class", "delete");
+      deleteButton.innerHTML = "Delete";
+
+      var analysis = document.createElement("div");
+      analysis.setAttribute("class", "analysis");
+      analysis.innerHTML = "Analyzing your recording...";
+
+      var li = document.createElement('li');
+      li.appendChild(audio);
+      li.appendChild(analysis);
+      li.appendChild(deleteButton);
+
+      var recordingsList = document.getElementById("recordingsList")
+
+      recordingsList.appendChild(li);
     }
+    
 
-    console.log(url);
-    audio.setAttribute("controls", "controls");
-    audio.setAttribute("src", url);
-    audio.setAttribute("type", "audio/wav");
-
-    var deleteButton = document.createElement("button");
-    deleteButton.setAttribute("class", "delete");
-    deleteButton.innerHTML = "Delete";
-
-    var analysis = document.createElement("div");
-    analysis.setAttribute("class", "analysis");
-    analysis.innerHTML = "Analyzing your recording...";
-
-    var li = document.createElement('li');
-    li.appendChild(audio);
-    li.appendChild(analysis);
-    li.appendChild(deleteButton);
-
-    var recordingsList = document.getElementById("recordingsList")
-
-    recordingsList.appendChild(li);
-
-    if (!file) {
+    if (!files) {
       console.error("No file selected");
       return;
     }
@@ -49,37 +55,46 @@ function handleOnChange() {
 
     xhr.open("POST", "http://localhost:8080/recording-api/file");
 
-    var formData = new FormData();
-    formData.append('file', file);
     xhr.send(formData);
 
     xhr.onload = function() {
-      var data = xhr.responseText;
-      var jsonResponse = JSON.parse(data);
-      var numHesitationsJSON = jsonResponse["numHesitations"];
-      var numWordsJSON = jsonResponse["numWords"];
-      var hesiTimesJSON = jsonResponse["hesiTimes"];
-      var accuracyJSON = ((numWordsJSON - numHesitationsJSON) / numWordsJSON) * 100;
+      if (xhr.status === 200) {
+        var data = xhr.response;
+        var jsonResponse = JSON.parse(data);
+        var list = document.getElementById("recordingsList");
+        var recordings = list.getElementsByTagName("li");
 
-      li.setAttribute("id", `${jsonResponse["id"]}`);
-      deleteButton.setAttribute("onclick", `handleDeleteOnClick(${jsonResponse["id"]})`);
-
-      var numHesitations = document.createElement("div");
-      numHesitations.setAttribute("class", "body");
-      numHesitations.innerHTML = `# of Hesitations: ${JSON.stringify(numHesitationsJSON)}`;
-
-      var accuracy = document.createElement("div");
-      accuracy.setAttribute("class", "body");
-      accuracy.innerHTML = `Accuracy: ${JSON.stringify(accuracyJSON)}%`;
-
-      var hesiTimes = document.createElement("div");
-      hesiTimes.setAttribute("class", "body");
-      hesiTimes.innerHTML = `Hesitation Timestamps (seconds): ${JSON.stringify(hesiTimesJSON)}`;
-
-      analysis.innerHTML = "";
-      analysis.appendChild(numHesitations);
-      analysis.appendChild(accuracy);
-      analysis.appendChild(hesiTimes);
+        for(var i = 0; i < jsonResponse.length; i++) {
+          console.log(jsonResponse[i]);
+          console.log(recordings[i]);
+          var numHesitationsJSON = jsonResponse[i]["numHesitations"];
+          var numWordsJSON = jsonResponse[i]["numWords"];
+          var hesiTimesJSON = jsonResponse[i]["hesiTimes"];
+          var accuracyJSON = ((numWordsJSON - numHesitationsJSON) / numWordsJSON) * 100;
+  
+          recordings[i].setAttribute("id", `${jsonResponse[i]["id"]}`);
+          deleteButton.setAttribute("onclick", `handleDeleteOnClick(${jsonResponse[i]["id"]})`);
+  
+          var numHesitations = document.createElement("div");
+          numHesitations.setAttribute("class", "body");
+          numHesitations.innerHTML = `# of Hesitations: ${JSON.stringify(numHesitationsJSON)}`;
+  
+          var accuracy = document.createElement("div");
+          accuracy.setAttribute("class", "body");
+          accuracy.innerHTML = `Accuracy: ${JSON.stringify(accuracyJSON)}%`;
+  
+          var hesiTimes = document.createElement("div");
+          hesiTimes.setAttribute("class", "body");
+          hesiTimes.innerHTML = `Hesitation Timestamps (seconds): ${JSON.stringify(hesiTimesJSON)}`;
+          
+          var analysisDiv = recordings[i].querySelector("div");
+          analysisDiv.innerHTML = "";
+          analysisDiv.appendChild(numHesitations);
+          analysisDiv.appendChild(accuracy);
+          analysisDiv.appendChild(hesiTimes);
+        }
+      }
+      
     }
   }
 
